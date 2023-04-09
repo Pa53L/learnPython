3b5af9bdc2fbc4bde13814b91cdfbabcf9f5095e
Pa5eL <pbeloshein@gmail.com>
2023-04-09 14:32:01 +0500

    init

---
+++ hello_world.py
@@ -0,0 +1,7 @@
+hello_world = "Hello fucking world!"
+counter = 0
+
+for i in range(0, 100, 2):
+    counter += 1
+    print(f"Моё послание миру: \"{hello_world}\"")
+    print(f"Ебучий счетчик напечатанных строк = {counter}")
