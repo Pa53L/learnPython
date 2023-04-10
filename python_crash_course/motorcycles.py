6e165d64735440faac69acb5a88022a723e7602d
Pa5eL <pbeloshein@gmail.com>
2023-04-10 06:24:54 +0500

    firstDay

--- python_crash_course/motorcycles.py
+++ python_crash_course/motorcycles.py
@@ -1,2 +1,36 @@
 motorcycles = ['honda', 'yamaha', 'suzuki']
 print(motorcycles)
+
+motorcycles[0] = 'ducati'
+print(motorcycles)
+
+motorcycles.append('honda')
+print(motorcycles)
+
+motorcycles.insert(1, 'Ural')
+print(motorcycles)
+
+del motorcycles[1]
+print(motorcycles)
+
+popped_motorcycle = motorcycles.pop()
+print(motorcycles)
+print(popped_motorcycle)
+
+first_owned = motorcycles.pop(0)
+print(motorcycles)
+print(f"The first motorcycle I owned was a {first_owned.title()}.")
+
+motorcycles.remove('yamaha')
+print(motorcycles)
+
+motorcycles.append('honda')
+motorcycles.append('Ural')
+motorcycles.append('yamaha')
+motorcycles.append('ducati')
+print(motorcycles)
+
+too_expensive = 'ducati'
+motorcycles.remove(too_expensive)
+print(motorcycles)
+print(f"\nA {too_expensive.title()} is too expensive for me.")
